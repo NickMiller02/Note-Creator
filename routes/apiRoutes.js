f6a64ed5c48386218e1f5794d560a96470588a7d
Nicholas Miller <nicholasmill157@gmail.com>
2022-12-19 21:31:16 -0500

    working on routes and server folders

---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+//Requirements
+const fs = require('fs');
+const storedData = require('../db/db.json');
+
+
+//Exporting api route function
+module.exports = function(app) {
+
+
+    //Takes data and puts it into DB(Database)
+
+    //Creating and saving note function
+
+    //writing note down to file
+
+    //display note from file
+
+    //add it to html
+
+
+};
